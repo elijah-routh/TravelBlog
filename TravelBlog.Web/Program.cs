4b49d4714cf3ea634b39531125af27228c013f87
elijah-routh <Elijahrouth@yahoo.com>
2026-07-15 22:35:24 -0400

    Working page using .Net

--- TravelBlog.Web/Program.cs
+++ TravelBlog.Web/Program.cs
@@ -1,25 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add MVC controllers and Razor views.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure production error handling.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+// Allows ASP.NET Core to serve files from wwwroot.
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
+// Default route:
+// / goes to HomeController.Index
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
